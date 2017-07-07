106cd3d1ea5a1b1a407517451529ed0d43284669
i.metlin <metlin.igor@gmail.com>
2017-07-07 17:02:04 +0300

    Metlin

---
+++ app/src/main/java/com/example/denfox/supermegaapp/IgorMetlin.java
@@ -0,0 +1,18 @@
+package com.example.denfox.supermegaapp;
+
+/**
+ * Created by i.metlin on 07.07.2017.
+ */
+
+public class IgorMetlin {
+
+    public IgorMetlin(){
+
+        int b = 99;
+        int a = 90;
+
+        System.out.println(a + b);
+
+    }
+
+}
--- app/src/main/java/com/example/denfox/supermegaapp/MainActivity.java
+++ app/src/main/java/com/example/denfox/supermegaapp/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     //TODO: Dont forget to declare your class here.
     private SomeSecondClass someSecondClass;
+    private IgorMetlin igorMetlin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
         initSomeClassStuff();
 
         //TODO: Put your classes-init here!!!
+        igorMetlin = new IgorMetlin();
+
+
 
     }
 
