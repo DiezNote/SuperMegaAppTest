25944aa884f7dba66d7e33d2e616701190f480f2
i.metlin <metlin.igor@gmail.com>
2017-07-07 17:40:01 +0300

    Metlin

--- app/src/main/java/com/example/denfox/supermegaapp/IgorMetlin.java
+++ app/src/main/java/com/example/denfox/supermegaapp/IgorMetlin.java
@@ -4,12 +4,17 @@ package com.example.denfox.supermegaapp;
  * Created by i.metlin on 07.07.2017.
  */
 
-public class IgorMetlin {
+public class IgorMetlin  {
 
     public IgorMetlin(){
 
+
+
         int b = 99;
         int a = 90;
+        boolean v = true;
+
+
 
         System.out.println(a + b);
 
