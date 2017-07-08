8456b3e21dcc6a34f738b987c0c2b7e9f6e63fc4
VSibi <win5783@gmail.com>
2017-07-08 10:19:36 +0300

    add class

---
+++ app/src/main/java/com/example/denfox/supermegaapp/Sibichenko.java
@@ -0,0 +1,11 @@
+package com.example.denfox.supermegaapp;
+
+import android.widget.Toast;
+
+public class Sibichenko {
+
+    public Sibichenko(){
+        Toast.makeText(Sibichenko.this, getResources().getString(R.string.hello), Toast.LENGTH_SHORT).show();
+    }
+
+}
