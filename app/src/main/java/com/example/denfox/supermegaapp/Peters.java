42c746cbeaeca91f7a653e3dae020c8d7e6d7d23
Peters <petersdimawork@gmail.com>
2017-07-08 18:59:59 +0300

    Peters Changes

--- app/src/main/java/com/example/denfox/supermegaapp/Peters.java
+++ app/src/main/java/com/example/denfox/supermegaapp/Peters.java
@@ -7,9 +7,22 @@ package com.example.denfox.supermegaapp;
 public class Peters {
     private String firstName;
     private String lastName;
+    private String fullName;
 
     public Peters() {
         firstName = "Dima";
         lastName = "Peters";
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
     }
 }
